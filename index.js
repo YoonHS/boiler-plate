db69b366e2653200e0899e558b1a07355a481bd2
YoonHS <hyungsunyoon@YoonHSPro.local>
2024-04-30 15:15:22 +0900

    add user sign on. and nodemon

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 5200
+const bodyParser = require('body-parser')
+const {User} = require('./models/User');
+
+app.use(express.json())
+
+// application/x-www-form-urlencoded parsing
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json parsing
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect(
@@ -9,9 +19,28 @@ mongoose.connect(
 ).then(() => console.log('MongoDB Connected..'))
 .catch(err => console.log(err))
 
-
 app.get('/', (req, res) => {
-  res.send('Hello World!  한글 ')
+  res.send('Hello World!  한글 goo')
+})
+
+app.post('/register', (req, res) => {
+  // user info send
+
+  const user = new User(req.body)
+  user
+    .save()
+    .then(()=>{
+      res.status(200).json({
+        success: true  
+      })
+    })
+    .catch((err)=>{
+      console.error(err);
+      res.json({
+        success:false,
+        err:err
+      })
+    })
 })
 
 app.listen(port, () => {
